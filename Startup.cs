85d184579580622658f80a6ed9df45c4ab140a83
geovaniDarcie <geovani_darcie@hotmail.com>
2021-05-31 21:38:29 -0300

    Adiciona o swagger na forma padrão

--- Startup.cs
+++ Startup.cs
@@ -41,7 +41,10 @@ namespace webapicurso
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "webapicurso v1"));
+                app.UseSwaggerUI(c => {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "webapicurso v1");
+                    c.RoutePrefix = string.Empty;
+                });
             }
 
             app.UseHttpsRedirection();
