81bff2a2bd21a0f22d14a60bf7fa74a8e2d0e7d9
geovaniDarcie <geovani_darcie@hotmail.com>
2021-05-31 21:38:41 -0300

    Create MovieController

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,36 @@
+
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("[controller]")] //movie
+public class MovieController : ControllerBase 
+{
+   public MovieController()
+   {
+
+   }
+
+   [HttpPost]
+   public string Post()
+   {
+       return "Post";
+   }
+
+   [HttpPut] 
+   public string Put()
+   {
+       return "Put";
+   }
+
+   [HttpGet]  
+   public string Get()
+   {
+       return "Get";
+   }
+
+   [HttpDelete] 
+   public string Delete()
+   {
+       return "Delete";
+   }
+}
