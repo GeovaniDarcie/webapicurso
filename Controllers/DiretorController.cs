6bbd6df4bf7ba8b133f92e2df8f9b2723558242a
geovanidarciesoitic <geovani.darcie@soitic.com>
2021-07-12 20:03:08 -0300

    Parametros corrigidos

--- Controllers/DiretorController.cs
+++ Controllers/DiretorController.cs
@@ -21,7 +21,6 @@ namespace webapicurso.Controllers
         /// <summary>
         /// Lista todos os diretores
         /// </summary>
-        /// <param name="nome">Nome do diretor</param>
         /// <returns>Lista dos diretores</returns>
         /// <response code="200">Sucesso ao retornar um diretor</response>
         /// <response code="404">Não existem diretores cadastrados</response>
@@ -51,7 +50,7 @@ namespace webapicurso.Controllers
         /// <summary>
         /// Busca um diretor
         /// </summary>
-        /// <param name="nome">Id do diretor</param>
+        /// <param name="id">Id do diretor</param>
         /// <returns>Lista dos diretores</returns>
         /// <response code="200">Sucesso ao retornar a lista dos diretores</response>
         /// <response code="404">Não existem diretores cadastrados</response>
@@ -84,7 +83,7 @@ namespace webapicurso.Controllers
         ///     }
         ///
         /// </remarks>
-        /// <param name="nome">Nome do diretor</param>
+        /// <param name="diretorInputPostDTO">Nome do diretor</param>
         /// <returns>O diretor criado</returns>
         /// <response code="200">Diretor foi criado com sucesso</response>
         /// <response code="400">Requisição sem corpo</response>
@@ -107,7 +106,7 @@ namespace webapicurso.Controllers
             return Ok(diretorOutputPostDTO);
         }
 
-         /// <summary>
+        /// <summary>
         /// Modificia os dados de um diretor
         /// </summary>
         /// <remarks>
@@ -115,17 +114,17 @@ namespace webapicurso.Controllers
         ///
         ///     PUT /diretor
         ///     {
-        ///        "nome": "Martin Scorsese",
+        ///        "nome": "Martin Scorsese"
         ///     }
         ///
         /// </remarks>
-        /// <param name="diretorId">Id do diretor</param>
-        /// <param name="nome">Nome do diretor</param>
+        /// <param name="id">Id do diretor</param>
+        /// <param name="diretorInputPutDTO">Dados do diretor</param>
         /// <returns>O diretor foi atualizado com sucesso</returns>
         /// <response code="200">Diretor foi atualizado</response>
         /// <response code="400">Requisição sem corpo</response>
         /// <response code="500">A solicitação não foi concluída devido a um erro interno no lado do servidor.</response>
-    
+
         [HttpPut]
         public async Task<ActionResult<DiretorOutputPutDTO>> Put(long id, [FromBody] DiretorInputPutDTO diretorInputPutDTO)
         {
@@ -148,12 +147,12 @@ namespace webapicurso.Controllers
         /// <summary>
         /// Deleta um diretor
         /// </summary>
-        /// <param name="diretorId">Id do diretor</param>
+        /// <param name="id">Id do diretor</param>
         /// <returns>Diretor deletado</returns>
         /// <response code="200">Diretor deletado com sucesso</response>
         /// <response code="404">Não existem diretores cadastrados para excluir</response>
         /// <response code="500">A solicitação não foi concluída devido a um erro interno no lado do servidor.</response>
-    
+
         [HttpDelete("{id}")]
         public async Task<ActionResult<Diretor>> Delete(long id)
         {
--- Controllers/FilmeController.cs
+++ Controllers/FilmeController.cs
@@ -55,15 +55,15 @@ namespace webapicurso.Controllers
             return filmesDto;
         }
 
-        
+
         /// <summary>
         /// Busca um filme
         /// </summary>
         /// <returns>Filme buscado com sucesso</returns>
-        /// <param name="titulo">Titulo do filme</param>
+        /// <param name="id">Filme id</param>
         /// <response code="200">Busca um filme</response>
         /// <response code="500">A solicitação não foi concluída devido a um erro interno no lado do servidor.</response>
-    
+
         [HttpGet("{id}")]
         public async Task<ActionResult<FilmeOutputGetByIdDTO>> Get(long id)
         {
@@ -100,10 +100,7 @@ namespace webapicurso.Controllers
         ///     }
         ///
         /// </remarks>
-        /// <param name="nome">Titulo do filme</param>
-        /// <param name="ano">Ano do filme</param>
-        /// <param name="genero">Gênero do filme</param>
-        /// <param name="diretor">Diretor do filme</param>
+        /// <param name="filmeInputPostDTO">Dados do filme</param>
         /// <returns>O filme foi criado</returns>
         /// <response code="200">Filme foi criado com sucesso</response>
         /// <response code="400">Requisição sem corpo</response>
@@ -158,9 +155,8 @@ namespace webapicurso.Controllers
         ///        "genero": "acao",
         ///     }
         /// </remarks>
-        /// <param name="nome">Titulo do filme</param>
-        /// <param name="ano">Ano do filme</param>
-        /// <param name="genero">Gênero do filme</param>
+        /// <param name="id">Id do filme</param>
+        /// <param name="filmeInputPutDTO">Dados de atualização do filme</param>
         /// <returns>O Filme foi atualizado</returns>
         /// <response code="200">Filme foi atualizado com sucesso</response>
         /// <response code="400">Requisição sem corpo</response>
@@ -202,7 +198,7 @@ namespace webapicurso.Controllers
         /// Deleta um filme
         /// </summary>
         /// <returns>Delete um filme</returns>
-        /// <param name="filmeId">Id do filme</param>
+        /// <param name="id">Id do filme</param>
         /// <response code="200">Filme excluído com sucesso</response>
         /// <response code="500">A solicitação não foi concluída devido a um erro interno no lado do servidor.</response>
 
