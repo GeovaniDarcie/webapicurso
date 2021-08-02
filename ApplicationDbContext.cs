200f00f4f3f9eafcc896ff635acfdf223a64158a
geovaniDarcie <geovani_darcie@hotmail.com>
2021-08-02 20:51:13 -0300

    Adiciona no dbcontext a configuracao de identity

--- ApplicationDbContext.cs
+++ ApplicationDbContext.cs
@@ -1,6 +1,7 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using webapicurso.Models;
-public class ApplicationDbContext : DbContext 
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 {
     public DbSet<Filme> Filmes { get; set; }
     public DbSet<Diretor> Diretores { get; set; }
